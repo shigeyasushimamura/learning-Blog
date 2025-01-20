17641d7a165159c917092ffdd38992c6431ab465
shigeyasushimamura <sgys.mtk@gmail.com>
2025-01-20 19:27:42 +0900

    ブログ真か

--- 2025_01_12_001/src/features/blog/BlogItem.tsx
+++ 2025_01_12_001/src/features/blog/BlogItem.tsx
@@ -30,6 +30,29 @@ const BlogItem = () => {
       </div>
       <h4 className={styles["blogitem__heading"]}>古き良き日本の冬へ❄</h4>
       <p className={styles["blogitem__date"]}>2025-01-19</p>
+      <hr />
+      <hr />
+      <hr />
+      <hr />
+      <hr />
+      <hr />
+      <div className={styles["blogitem__frame"]}>
+        <div className={styles["blogitem__video-container"]}>
+          <iframe
+            width="560"
+            height="315"
+            className={styles["blogitem__video"]}
+            src="https://www.youtube.com/embed/tPTFTmVdQJQ?si=6d9X1rg8PFu5jy_F"
+            title="YouTube video player"
+            frameborder="0"
+            allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+            referrerpolicy="strict-origin-when-cross-origin"
+            allowfullscreen
+          ></iframe>
+        </div>
+      </div>
+      <h4 className={styles["blogitem__heading"]}>古き良き日本の冬へ❄</h4>
+      <p className={styles["blogitem__date"]}>2025-01-19</p>
     </div>
   );
 };
